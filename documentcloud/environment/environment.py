972f0444718426a5b0f43848c887e03069d94593
Mitchell Kotler <mitch@mitchellkotler.net>
2019-11-08 09:56:45 -0500

    add test env

--- documentcloud/environment/environment.py
+++ documentcloud/environment/environment.py
@@ -126,6 +126,11 @@ elif environment == "local":
     from documentcloud.environment.pubsub import publisher
     from documentcloud.environment.httpsub import httpsub
 
+elif environment == "local-test":
+    from documentcloud.environment.storage import storage
+    from documentcloud.environment.pubsub import publisher
+    from documentcloud.environment.httpsub import httpsub
+
 elif environment == "aws":
     import requests as httpsub
 
