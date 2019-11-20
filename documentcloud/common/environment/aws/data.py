21cdc6e78175602c197a9e6e4f6d16d550d33035
Dylan Freedman <freedmand@gmail.com>
2019-11-20 20:28:11 +0000

    Add data methods specialized by environment

--- documentcloud/common/environment/aws/data.py
+++ documentcloud/common/environment/aws/data.py
@@ -2,25 +2,17 @@
 import base64
 import json
 
-# XXX specialize to this environment
-
 
 def get_http_data(request):
-    """Extract data from an HTTP request that works across environments."""
-    if hasattr(request, "get_json"):
-        # Object is a request object
-        return request.get_json()
-    elif "body" in request:
-        # Object is an AWS LambdaContext object
-        return json.loads(request["body"])
-    else:
-        # Object is plain JSON
-        return request
+    """Extract data from an HTTP request."""
+    return json.loads(request["body"])
 
 
 def get_pubsub_data(data):
-    """Extract data from a pubsub request that works across environments."""
-    if "Records" in data:
-        return json.loads(data["Records"][0]["Sns"]["Message"])
-    else:
-        return json.loads(base64.b64decode(data["data"]).decode("utf-8"))
+    """Extract data from a pubsub request."""
+    return json.loads(data["Records"][0]["Sns"]["Message"])
+
+
+def encode_pubsub_data(data):
+    """Encode data into the proper format for a pubsub request."""
+    return json.dumps(data).encode("utf8")
