721b2a0f683973c3c12c56b72bd044261cf249a6
Dylan Freedman <freedmand@gmail.com>
2019-12-04 05:22:47 +0000

    Fix up environment variables and build scripts to ignore permissions errors

--- config/aws/lambda/build_info_and_image.sh
+++ config/aws/lambda/build_info_and_image.sh
@@ -10,7 +10,7 @@ CODE_DIR=awsbin/info_and_image
 [ -d "$CODE_DIR" ] || mkdir -p $CODE_DIR
 
 # Copy the code from the Django app
-cp -Lr ../../../documentcloud/documents/processing/info_and_image/* $CODE_DIR
+cp -Lr ../../../documentcloud/documents/processing/info_and_image/* $CODE_DIR 2>/dev/null || :
 
 # Set AWS requirements
 cp cloud-requirements.txt $CODE_DIR/cloud-requirements.txt
