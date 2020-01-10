1a1a14b84c515865f5432e4df428e4f2999d8b7e
Dylan Freedman <freedmand@gmail.com>
2020-01-10 15:17:34 +0000

    Use relative import in aws

--- documentcloud/common/environment/aws/httpsub.py
+++ documentcloud/common/environment/aws/httpsub.py
@@ -1,10 +1,1 @@
-# Third Party
-import environ
-
-env = environ.Env()
-
-# Imports based on execution context
-if env.str("ENVIRONMENT").startswith("local"):
-    from documentcloud.common.session import session as httpsub
-else:
-    from common.session import session as httpsub
+from ...session import session as httpsub
