896edc0c14589c967d6879acacc4dbaf3a9b06d6
Mitchell Kotler <mitch@mitchellkotler.net>
2020-01-10 11:05:22 -0500

    update sam option name

--- config/aws/lambda/deploy.sh
+++ config/aws/lambda/deploy.sh
@@ -6,5 +6,5 @@ set -e
 python3 replace_params.py && \
 ./build.sh && \
 sam build && \
-sam package --output-template packaged.yaml --s3-bucket cloud-functions-test && \
+sam package --output-template-file packaged.yaml --s3-bucket cloud-functions-test && \
 sam deploy --template-file packaged.yaml --region us-east-1 --capabilities CAPABILITY_IAM --stack-name info-and-image-staging
