5a3510ee6044933b00be5edb2fb5fb4109e75ee6
Mitchell Kotler <mitch@mitchellkotler.net>
2020-02-19 15:46:20 -0500

    paramatize lambda deploy script

--- config/aws/lambda/deploy.sh
+++ config/aws/lambda/deploy.sh
@@ -7,4 +7,4 @@ python3 replace_params.py && \
 ./build.sh && \
 sam build && \
 sam package --output-template-file packaged.yaml --s3-bucket cloud-functions-test && \
-sam deploy --template-file packaged.yaml --region us-east-1 --capabilities CAPABILITY_IAM --stack-name info-and-image-staging
+sam deploy --template-file packaged.yaml --region us-east-1 --capabilities CAPABILITY_IAM --stack-name $1
