bd854f21de6d898598a9dd4347a165de4fa54907
Mitchell Kotler <mitch@mitchellkotler.net>
2020-02-20 10:59:34 -0500

    increase max pagination size

--- documentcloud/core/pagination.py
+++ documentcloud/core/pagination.py
@@ -5,4 +5,4 @@ from rest_framework import pagination
 class PageNumberPagination(pagination.PageNumberPagination):
     page_size = 25
     page_size_query_param = "per_page"
-    max_page_size = 100
+    max_page_size = 1000
