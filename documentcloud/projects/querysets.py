a71aea98c588d478d3221a835dee6e05e22784e5
Mitchell Kotler <mitch@mitchellkotler.net>
2020-03-31 14:15:30 -0400

    performance improvements

--- documentcloud/projects/querysets.py
+++ documentcloud/projects/querysets.py
@@ -3,9 +3,13 @@
 # Django
 from django.db import models
 from django.db.models import Q
+from django.db.models.expressions import OuterRef, Subquery, Value
+from django.db.models.fields import BooleanField
+from django.db.models.functions import Cast
 
 # DocumentCloud
 from documentcloud.documents.models import Document
+from documentcloud.projects.choices import CollaboratorAccess
 
 
 class ProjectQuerySet(models.QuerySet):
@@ -16,6 +20,26 @@ class ProjectQuerySet(models.QuerySet):
             return self.filter(Q(private=False) | Q(collaborators=user)).distinct()
         else:
             return self.filter(private=False)
+
+    def annotate_is_admin(self, user):
+        """Annotate each project with whether or not the given user is
+        an admin for it
+        """
+        if user.is_authenticated:
+            return self.annotate(
+                is_admin=Cast(
+                    Subquery(
+                        user.collaboration_set.filter(
+                            project_id=OuterRef("pk"), access=CollaboratorAccess.admin
+                        )
+                        .values("pk")
+                        .order_by()
+                    ),
+                    output_field=BooleanField(),
+                )
+            )
+        else:
+            return self.annotate(is_admin=Value(False, output_field=BooleanField()))
 
 
 class ProjectMembershipQuerySet(models.QuerySet):
