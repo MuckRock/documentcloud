b308226d371096aaf3ef4f7e77fcf147ae42a036
Mitchell Kotler <mitch@mitchellkotler.net>
2020-05-04 16:23:31 -0400

    allow searching by user if they have a public document close #69

--- documentcloud/users/querysets.py
+++ documentcloud/users/querysets.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db.models import Prefetch, Q
 
 # DocumentCloud
+from documentcloud.documents.choices import Access
 from documentcloud.organizations.models import Membership
 
 
@@ -12,14 +13,17 @@ class UserQuerySet(models.QuerySet):
     """Custom queryset for users"""
 
     def get_viewable(self, user):
-        """You may view other users in your organizations and projects"""
+        """You may view other users in your organizations and projects,
+        and anybody with a public document
+        """
         if user.is_authenticated:
             return self.filter(
                 Q(organizations__in=user.organizations.all())
                 | Q(projects__in=user.projects.all())
+                | Q(documents__access=Access.public)
             ).distinct()
         else:
-            return self.none()
+            return self.filter(documents__access=Access.public).distinct()
 
     def preload(self, _user=None, _expand=""):
         """Preload relations"""
--- documentcloud/users/rules.py
+++ documentcloud/users/rules.py
@@ -5,6 +5,7 @@ from rules import add_perm, always_deny, is_authenticated, predicate
 
 # DocumentCloud
 from documentcloud.core.rules import skip_if_not_obj
+from documentcloud.documents.choices import Access
 from documentcloud.organizations.models import Organization
 from documentcloud.projects.models import Project
 
@@ -31,7 +32,15 @@ def is_me(user, user_):
     return user == user_
 
 
-can_view = is_authenticated & (is_organization | is_collaborator)
+@predicate
+@skip_if_not_obj
+def has_public_document(user, user_):
+    user_.documents.filter(access=Access.public).exists()
+
+
+can_view = has_public_document | (
+    is_authenticated & (is_organization | is_collaborator)
+)
 can_change = is_authenticated & is_me
 
 add_perm("users.view_user", can_view)
