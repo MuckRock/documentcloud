1695ca3852479220d5a89f0522095511c830493b
Mitchell Kotler <mitch@mitchellkotler.net>
2020-05-28 12:07:24 -0400

    admin updates

--- documentcloud/organizations/admin.py
+++ documentcloud/organizations/admin.py
@@ -1,68 +1,24 @@
 # Django
 from django.contrib import admin
-from django.urls import reverse
-from django.utils.safestring import mark_safe
 
 # Third Party
 from reversion.admin import VersionAdmin
+from squarelet_auth.organizations.admin import OrganizationAdmin as SAOrganizationAdmin
 
 # DocumentCloud
 from documentcloud.organizations.models import Organization
-from documentcloud.users.models import User
 
 
 @admin.register(Organization)
-class OrganizationAdmin(VersionAdmin):
+class OrganizationAdmin(VersionAdmin, SAOrganizationAdmin):
     """Organization Admin"""
 
-    list_display = ("name", "entitlement", "private", "individual")
-    list_filter = ("entitlement", "private", "individual")
-    search_fields = ("name", "users__username")
-    fields = (
-        "uuid",
-        "name",
-        "slug",
-        "private",
-        "individual",
-        "entitlement",
-        "card",
+    fields = SAOrganizationAdmin.fields + (
         "pages_per_month",
         "monthly_pages",
         "number_pages",
-        "date_update",
+        "language",
+        "document_language",
     )
-    readonly_fields = (
-        "uuid",
-        "name",
-        "slug",
-        "private",
-        "individual",
-        "entitlement",
-        "card",
-        "pages_per_month",
-        "date_update",
-    )
+    readonly_fields = SAOrganizationAdmin.readonly_fields + ("pages_per_month",)
     list_select_related = ("entitlement",)
-
-    def get_fields(self, request, obj=None):
-        """Only add user link for individual organizations"""
-        if obj and obj.individual:
-            return ("user_link",) + self.fields
-        else:
-            return self.fields
-
-    def get_readonly_fields(self, request, obj=None):
-        """Only add user link for individual organizations"""
-        if obj and obj.individual:
-            return ("user_link",) + self.readonly_fields
-        else:
-            return self.readonly_fields
-
-    @mark_safe
-    def user_link(self, obj):
-        """Link to the individual org's user"""
-        user = User.objects.get(uuid=obj.uuid)
-        link = reverse("admin:users_user_change", args=(user.pk,))
-        return '<a href="%s">%s</a>' % (link, user.username)
-
-    user_link.short_description = "User"
--- documentcloud/users/admin.py
+++ documentcloud/users/admin.py
@@ -1,63 +1,14 @@
 # Django
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from django.urls import reverse
-from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
 
 # Third Party
 from reversion.admin import VersionAdmin
+from squarelet_auth.users.admin import UserAdmin as SAUserAdmin
 
 # DocumentCloud
 from documentcloud.users.models import User
 
 
 @admin.register(User)
-class UserAdmin(VersionAdmin, AuthUserAdmin):
-    fieldsets = (
-        (None, {"fields": ("uuid", "username", "org_link")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "created_at", "updated_at")}),
-    )
-    readonly_fields = (
-        "uuid",
-        "username",
-        "org_link",
-        "name",
-        "email",
-        "last_login",
-        "created_at",
-        "updated_at",
-    )
-    list_display = (
-        "username",
-        "name",
-        "email",
-        "is_staff",
-        "is_superuser",
-        "is_active",
-    )
-    search_fields = ("username", "name", "email")
-
-    @mark_safe
-    def org_link(self, obj):
-        """Link to the individual org"""
-        link = reverse(
-            "admin:organizations_organization_change",
-            args=(obj.individual_organization.pk,),
-        )
-        return '<a href="%s">%s</a>' % (link, obj.individual_organization.name)
-
-    org_link.short_description = "Individual Organization"
+class UserAdmin(VersionAdmin, SAUserAdmin):
+    """User Admin"""
