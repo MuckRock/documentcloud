d89bf803dc1690a30d1165e5a1562b2b143ba585
Dylan Freedman <freedmand@gmail.com>
2020-09-09 15:08:44 +0000

    Refactor to get all OCR languages

--- config/aws/lambda/build.sh
+++ config/aws/lambda/build.sh
@@ -5,8 +5,13 @@ set -e
 # Run info_and_image build script
 ./build_info_and_image.sh
 
-# Run ocr build script
-./build_ocr.sh
+# Get all languages
+languages=($(DJANGO_SETTINGS_MODULE=config.settings.local python -c "from django.core.wsgi import get_wsgi_application;application = get_wsgi_application();from documentcloud.core.choices import Language;print(*[Language.ocr_name(x[0]) for x in Language.choices])"))
+# Run ocr build script for each language
+for lang in $languages
+do
+    ./build_ocr.sh $lang
+done
 
 # Run utils build script
 ./build_utils.sh
--- config/aws/lambda/build_ocr.sh
+++ config/aws/lambda/build_ocr.sh
@@ -2,19 +2,31 @@
 
 set -e
 
-CODE_DIR=awsbin/ocr
+LANG=$1
+CODE_DIR="awsbin/ocr_${LANG}"
+OCR_DIRECTORY="../../../documentcloud/documents/processing/ocr"
 
 # Clear the code directory if it already exists
 [ -d "$CODE_DIR" ] && rm -Rf $CODE_DIR
 # Make the code directory if it does not exist
 [ -d "$CODE_DIR" ] || mkdir -p $CODE_DIR
 
+# Fetch OCR language if not already present
+OCR_FILE="${OCR_DIRECTORY}/tesseract/tessdata/${LANG}.traineddata"
+if [ ! -f "$FILE" ]; then
+    TESSDATA_URL="https://github.com/tesseract-ocr/tessdata/raw/4.00/${LANG}.traineddata"
+    wget -O OCR_FILE TESSDATA_URL
+fi
+
 # Copy the code from the Django app
-cp -Lr ../../../documentcloud/documents/processing/ocr/* $CODE_DIR 2>/dev/null || :
+cp -Lr "${OCR_DIRECTORY}/*" $CODE_DIR 2>/dev/null || :
 
 # Sub in Amazon Linux compiled Tesseract libraries
 rm -r $CODE_DIR/tesseract
 cp -r ocr_libraries/ $CODE_DIR/tesseract 2>/dev/null || :
+
+# Remove all Tesseract languages but the desired one
+find "${CODE_DIR}/tesseract/tessdata" ! -name "${LANG}.traineddata" -type f -exec rm -f {} +
 
 # Set AWS requirements
 cp cloud-requirements.txt $CODE_DIR/cloud-requirements.txt
