d6211d626b313b20e42b7cd704709711ccfd85b5
Mitchell Kotler <mitch@mitchellkotler.net>
2020-11-03 10:29:39 -0500

    make oembed regexs unicode aware

--- documentcloud/documents/oembed.py
+++ documentcloud/documents/oembed.py
@@ -19,9 +19,7 @@ class DocumentOEmbed(RichOEmbed):
     template = "oembed/document.html"
     patterns = [
         # viewer url
-        re.compile(
-            rf"^{settings.DOCCLOUD_URL}/documents/(?P<pk>[0-9]+)[A-Za-z0-9_-]*/?$"
-        ),
+        re.compile(rf"^{settings.DOCCLOUD_URL}/documents/(?P<pk>[0-9]+)[\w-]*/?$"),
         # api url
         re.compile(rf"^{settings.DOCCLOUD_API_URL}/api/documents/(?P<pk>[0-9]+)/?$"),
     ]
@@ -69,7 +67,7 @@ class PageOEmbed(DocumentOEmbed):
     patterns = [
         re.compile(
             rf"^{settings.DOCCLOUD_URL}/documents/"
-            r"(?P<pk>[0-9]+)[A-Za-z0-9_-]*/?#document/p(?P<page>[0-9]+)$"
+            r"(?P<pk>[0-9]+)[\w-]*/?#document/p(?P<page>[0-9]+)$"
         )
     ]
 
@@ -107,7 +105,7 @@ class NoteOEmbed(RichOEmbed):
     template = "oembed/note.html"
     patterns = [
         re.compile(
-            rf"^{settings.DOCCLOUD_URL}/documents/(?P<doc_pk>[0-9]+)[A-Za-z0-9_-]*/?"
+            rf"^{settings.DOCCLOUD_URL}/documents/(?P<doc_pk>[0-9]+)[\w-]*/?"
             r"#document/p(?P<page>[0-9]+)/a(?P<pk>[0-9]+)$"
         )
     ]
