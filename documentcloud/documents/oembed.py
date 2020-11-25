27d3670a09044cb28403ff4e5f1d669416d0288b
Dylan Freedman <freedmand@gmail.com>
2020-11-25 18:42:41 +0000

    Oembed for project embeds

--- documentcloud/documents/oembed.py
+++ documentcloud/documents/oembed.py
@@ -10,6 +10,7 @@ import time
 # DocumentCloud
 from documentcloud.common.path import page_image_path, page_text_path
 from documentcloud.documents.models import Document
+from documentcloud.projects.models import Project
 from documentcloud.oembed.oembed import RichOEmbed
 from documentcloud.oembed.registry import register
 
@@ -139,3 +140,46 @@ class NoteOEmbed(RichOEmbed):
         template = get_template(self.template)
         oembed["html"] = template.render(context)
         return self.oembed(**oembed)
+
+
+@register
+class ProjectOEmbed(RichOEmbed):
+    template = "oembed/project.html"
+    patterns = [
+        # manager url
+        re.compile(
+            rf"^{settings.DOCCLOUD_URL}/projects/[\w-]*?(?P<proj_pk>[0-9]+)(/|\?|$)"
+        ),
+        # api url
+        re.compile(
+            rf"^{settings.DOCCLOUD_API_URL}/api/projects/[\w-]*?(?P<proj_pk>[0-9]+)(/|\?|$)$"
+        ),
+    ]
+    width = 500
+    height = 500
+
+    def response(self, request, query, max_width=None, max_height=None, **kwargs):
+        project = get_object_or_404(
+            Project.objects.get_viewable(request.user), pk=kwargs["proj_pk"]
+        )
+
+        if max_width and max_width < self.width:
+            width = max_width
+        else:
+            width = self.width
+        if max_height and max_height < self.height:
+            height = max_height
+        else:
+            height = self.height
+        oembed = {"title": project.title, "width": width, "height": height}
+        context = self.get_context(project, query, oembed, **kwargs)
+        template = get_template(self.template)
+        oembed["html"] = template.render(context)
+        return self.oembed(**oembed)
+
+    def get_context(self, project, query, extra, **kwargs):
+        # pylint: disable=unused-argument
+        src = settings.DOCCLOUD_EMBED_URL + project.get_absolute_url()
+        if query:
+            src = f"{src}?{query}"
+        return {"src": src, **extra}
