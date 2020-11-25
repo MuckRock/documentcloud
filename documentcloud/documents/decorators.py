07df50f0725f5680e51c6f333c00487a3feef7f9
Mitchell Kotler <mitch@mitchellkotler.net>
2020-11-25 16:14:19 -0500

    add more caching for various embed types

--- documentcloud/documents/decorators.py
+++ documentcloud/documents/decorators.py
@@ -1,5 +1,7 @@
 # Django
+from django.conf import settings
 from django.utils.cache import patch_cache_control
+from django.views.decorators.vary import vary_on_cookie
 
 # Standard Library
 from functools import wraps
@@ -22,3 +24,22 @@ def conditional_cache_control(**kwargs):
         return _cache_controlled
 
     return _cache_controller
+
+
+def anonymous_cache_control(viewfunc):
+    """Cache this view only if the user is anonymous"""
+
+    @wraps(viewfunc)
+    @vary_on_cookie
+    def inner(request, *args, **kwargs):
+        response = viewfunc(request, *args, **kwargs)
+        has_auth_token = hasattr(request, "auth") and request.auth is not None
+        if has_auth_token or request.user.is_authenticated:
+            patch_cache_control(response, private=True, no_cache=True)
+        else:
+            patch_cache_control(
+                response, public=True, max_age=settings.CACHE_CONTROL_MAX_AGE
+            )
+        return response
+
+    return inner
