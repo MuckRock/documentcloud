8fb46df0158ef94550b456ccf7d8e370f6bf9cb6
Mitchell Kotler <mitch@mitchellkotler.net>
2020-12-03 13:47:07 -0500

    dont let deleting files time out

--- documentcloud/documents/tasks.py
+++ documentcloud/documents/tasks.py
@@ -128,9 +128,11 @@ def process_cancel(document_pk):
     )
 
 
-@task
+@task(autoretry_for=(SoftTimeLimitExceeded,))
 def delete_document_files(path):
-    # delete files from storage
+    """Delete all of the files from storage for the given path"""
+    # For AWS, can delete 1000 files at a time - if we hit the time limit,
+    # just retry - it will continue deleting files from the path where it left off
     storage.delete(path)
 
 
