236fceba8afcfd5b211ef48bb5f114a9de30f5ae
Mitchell Kotler <mitch@mitchellkotler.net>
2020-12-17 15:56:48 -0500

    obey byte limit for google entity extraction

--- documentcloud/documents/entity_extraction.py
+++ documentcloud/documents/entity_extraction.py
@@ -1,16 +1,20 @@
 # Third Party
+# Standard Library
+import logging
+
 from google.cloud import language_v1
 from google.cloud.language_v1.types.language_service import AnalyzeEntitiesResponse
 
 # DocumentCloud
 from documentcloud.documents.models import Entity, EntityOccurence
 
+TEXT_LIMIT = 1000000
 
-def extract_entities(document):
-    """Extract the entities from a document"""
-    # XXX ensure no entities yet? or clear existing?
-    client = language_v1.LanguageServiceClient()
-    text = document.get_text()
+logger = logging.getLogger(__name__)
+client = language_v1.LanguageServiceClient()
+
+
+def _extract_entities_text(document, text):
     language_document = language_v1.Document(
         content=text, type_=language_v1.Document.Type.PLAIN_TEXT
     )
@@ -29,3 +33,31 @@ def extract_entities(document):
             relevance=entity["salience"],
             occurences=entity["mentions"],
         )
+
+
+def extract_entities(document):
+    """Extract the entities from a document"""
+    # XXX ensure no entities yet? or clear existing?
+
+    page_text = document.get_all_page_text()
+    texts = []
+    total_len = 0
+
+    for page in page_text["pages"]:
+        page_len = len(page["content"].encode("utf8"))
+        if page_len > TEXT_LIMIT:
+            logger.error("Single page too long for entity extraction")
+            return
+
+        if total_len + page_len > TEXT_LIMIT:
+            # if adding another page would put us over the limit,
+            # send the current chunk of text to be analyzed
+            _extract_entities_text(document, "".join(texts))
+            texts = []
+        else:
+            # otherwise append the current page and accumulate the length
+            texts.append(page["content"])
+            total_len += page_len
+
+    # analyze the remaining text
+    _extract_entities_text(document, "".join(texts))
