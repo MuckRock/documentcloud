b097316b3cd13a7b0346b5bf6e59bbe7de22f943
Mitchell Kotler <mitch@mitchellkotler.net>
2021-01-19 14:05:22 -0500

    remove path check for anonymous get cors

--- documentcloud/core/signals.py
+++ documentcloud/core/signals.py
@@ -1,21 +1,12 @@
 # Django
-from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
 from django.core.cache import cache
 from django.core.cache.utils import make_template_fragment_key
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Standard Library
-import logging
-import re
-
 # Third Party
 from corsheaders.signals import check_request_enabled
-
-ALLOW_PATHS = [re.compile(p) for p in settings.CORS_ALLOW_PATHS]
-
-logger = logging.getLogger(__name__)
 
 
 @receiver(
@@ -34,8 +25,4 @@ def check_cors(sender, request, **kwargs):
     """Allow anonymous GET/OPTIONS requests to the pre-defined allowed paths"""
     # pylint: disable=unused-argument
     anonymous = not hasattr(request, "user") or request.user.is_anonymous
-    return (
-        request.method.lower() in ["get", "options"]
-        and anonymous
-        and any(p.match(request.path) for p in ALLOW_PATHS)
-    )
+    return request.method.lower() in ["get", "options"] and anonymous
