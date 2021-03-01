eabda25ae81115bc17b072ace890dbecf618c76c
Mitchell Kotler <mitch@mitchellkotler.net>
2021-03-01 16:48:10 -0500

    moesif metadata integration

---
+++ documentcloud/organizations/signals.py
@@ -0,0 +1,31 @@
+# Django
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Third Party
+from moesifapi.moesif_api_client import MoesifAPIClient
+
+# DocumentCloud
+from documentcloud.organization.models import Organization
+
+if hasattr(settings, "MOESIF_MIDDLEWARE"):
+    api_client = MoesifAPIClient(settings.MOESIF_MIDDLEWARE["APPLICATION_ID"]).api
+
+    @receiver(
+        post_save,
+        sender=Organization,
+        dispatch_uid="documentcloud.user.signals.moesif_organization",
+    )
+    def moesif_organization(instance, **_kwargs):
+        api_client.update_company(
+            {
+                "company_id": str(instance.pk),
+                "metadata": {
+                    "name": instance.name,
+                    "photo_url": instance.avatar_url,
+                    "plan": instance.entitlement.name if instance.entitlement else None,
+                    "verified": instance.verified_journalist,
+                },
+            }
+        )
---
+++ documentcloud/users/signals.py
@@ -0,0 +1,29 @@
+# Django
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Third Party
+from moesifapi.moesif_api_client import MoesifAPIClient
+
+# DocumentCloud
+from documentcloud.users.models import User
+
+if hasattr(settings, "MOESIF_MIDDLEWARE"):
+    api_client = MoesifAPIClient(settings.MOESIF_MIDDLEWARE["APPLICATION_ID"]).api
+
+    @receiver(
+        post_save, sender=User, dispatch_uid="documentcloud.user.signals.moesif_user"
+    )
+    def moesif_user(instance, **_kwargs):
+        api_client.update_user(
+            {
+                "user_id": str(instance.pk),
+                "company_id": str(instance.organization.pk),
+                "metadata": {
+                    "email": instance.email,
+                    "name": instance.name,
+                    "photo_url": instance.avatar_url,
+                },
+            }
+        )
