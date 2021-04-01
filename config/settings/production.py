08e42a0769a49abcf23a4ed1cf73596b0336ad3e
Mitchell Kotler <mitch@mitchellkotler.net>
2021-04-01 15:25:38 -0400

    moesif

--- config/settings/production.py
+++ config/settings/production.py
@@ -241,22 +241,24 @@ SCOUT_NAME = env("SCOUT_NAME")
 
 # Moesif
 # ------------------------------------------------------------------------------
-MIDDLEWARE += ["moesifdjango.middleware.moesif_middleware"]
-INSTALLED_APPS += ["moesifdjango"]
-MOESIF_MIDDLEWARE = {
-    "APPLICATION_ID": env("MOESIF_ID"),
-    "LOG_BODY": True,
-    "LOCAL_DEBUG": env.bool("MOESIF_LOCAL_DEBUG", default=False),
-    "USE_CELERY": True,
-    "CELERY_BROKER_URL": CELERY_BROKER_URL,
-    "SKIP": lambda request, response: request.headers.get("referer", "").startswith(
-        DOCCLOUD_EMBED_URL
-    )
-    or request.headers.get("origin", "").startswith(DOCCLOUD_EMBED_URL),
-    "IDENTIFY_USER": lambda request, response: request.user.pk
-    if request.user and request.user.is_authenticated
-    else None,
-    "IDENTIFY_COMPANY": lambda request, response: request.user.organization.pk
-    if request.user and request.user.is_authenticated
-    else None,
-}
+MOESIF_ID = env("MOESIF_ID", default=None)
+if MOESIF_ID:
+    MIDDLEWARE += ["moesifdjango.middleware.moesif_middleware"]
+    INSTALLED_APPS += ["moesifdjango"]
+    MOESIF_MIDDLEWARE = {
+        "APPLICATION_ID": MOESIF_ID,
+        "LOG_BODY": True,
+        "LOCAL_DEBUG": env.bool("MOESIF_LOCAL_DEBUG", default=False),
+        "USE_CELERY": True,
+        "CELERY_BROKER_URL": CELERY_BROKER_URL,
+        "SKIP": lambda request, response: request.headers.get("referer", "").startswith(
+            DOCCLOUD_EMBED_URL
+        )
+        or request.headers.get("origin", "").startswith(DOCCLOUD_EMBED_URL),
+        "IDENTIFY_USER": lambda request, response: request.user.pk
+        if request.user and request.user.is_authenticated
+        else None,
+        "IDENTIFY_COMPANY": lambda request, response: request.user.organization.pk
+        if request.user and request.user.is_authenticated
+        else None,
+    }
