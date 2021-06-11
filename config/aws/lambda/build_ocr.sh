99bd6c3dd400b88bdf22b9b7874537586893112a
Dylan Freedman <freedmand@gmail.com>
2021-06-11 03:54:58 +0000

    Fix build ocr script

--- config/aws/lambda/build_ocr.sh
+++ config/aws/lambda/build_ocr.sh
@@ -2,26 +2,13 @@
 
 set -e
 
-LANGUAGES=$@
-LANGUAGES_FN=$(echo $LANGUAGES | tr ' ' '_')
-CODE_DIR="awsbin/ocr_${LANGUAGES_FN}"
+CODE_DIR="awsbin/ocr"
 OCR_DIRECTORY="../../../documentcloud/documents/processing/ocr"
 
 # Clear the code directory if it already exists
 [ -d "$CODE_DIR" ] && rm -Rf $CODE_DIR
 # Make the code directory if it does not exist
 [ ! -d "$CODE_DIR" ] || mkdir -p $CODE_DIR
-
-# Fetch OCR language if not already present
-for LANG in $LANGUAGES
-do
-    OCR_FILE="${OCR_DIRECTORY}/tesseract/tessdata/${LANG}.traineddata"
-    if [ ! -f "$OCR_FILE" ]; then
-        mkdir -p "$(dirname $OCR_FILE)"
-        TESSDATA_URL="https://github.com/tesseract-ocr/tessdata/raw/4.00/${LANG}.traineddata"
-        wget -O "${OCR_FILE}" "${TESSDATA_URL}"
-    fi
-done
 
 # Copy the code from the Django app, excluding tesseract data
 rsync -aL "${OCR_DIRECTORY}/" $CODE_DIR --exclude tesseract
@@ -30,13 +17,6 @@ rsync -aL "${OCR_DIRECTORY}/" $CODE_DIR --exclude tesseract
 [ -f $CODE_DIR/tesseract ] && rm -r $CODE_DIR/tesseract
 cp -r ocr_libraries/ $CODE_DIR/tesseract 2>/dev/null || :
 mkdir $CODE_DIR/tesseract/tessdata
-
-# Copy in just the desired tessdata language
-for LANG in $LANGUAGES
-do
-    OCR_FILE="${OCR_DIRECTORY}/tesseract/tessdata/${LANG}.traineddata"
-    cp "${OCR_FILE}" "${CODE_DIR}/tesseract/tessdata/${LANG}.traineddata"
-done
 
 # Set AWS requirements
 cp cloud-requirements.txt $CODE_DIR/cloud-requirements.txt
