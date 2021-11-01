e0c6f46c5629beacbed4683de258fb19ee49160b
Mitchell Kotler <mitch@mitchellkotler.net>
2021-11-01 09:34:29 -0400

    session cookie setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -429,7 +429,7 @@ LOGIN_URL = "/accounts/login/squarelet"
 LOGIN_REDIRECT_URL = DOCCLOUD_URL + "/app"
 LOGOUT_REDIRECT_URL = DOCCLOUD_URL
 # This lets us send the session cookie to the API
-SESSION_COOKIE_SAMESITE = None
+SESSION_COOKIE_SAMESITE = "None"
 
 SIMPLE_JWT = {
     "ALGORITHM": "RS256",
