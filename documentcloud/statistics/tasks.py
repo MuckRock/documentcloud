b97afa951646eae57257ac240fe22916b03586e6
Mitchell Kotler <mitch@mitchellkotler.net>
2022-01-05 12:11:21 -0500

    optimize stat storing

--- documentcloud/statistics/tasks.py
+++ documentcloud/statistics/tasks.py
@@ -4,7 +4,9 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 from django.core.management import call_command
 from django.db.models import Sum
+from django.db.models.aggregates import Count
 from django.db.models.functions import Coalesce
+from django.db.models.query_utils import Q
 
 # Standard Library
 import logging
@@ -24,43 +26,33 @@ def store_statistics():
     """Store the daily statistics"""
     # pylint: disable=too-many-statements
 
+    logger.info("[STORE STATS] Begin")
+
     yesterday = date.today() - timedelta(1)
 
     kwargs = {}
     kwargs["date"] = yesterday
 
-    kwargs["total_documents"] = Document.objects.count()
-    kwargs["total_documents_public"] = Document.objects.filter(
-        access=Access.public
-    ).count()
-    kwargs["total_documents_organization"] = Document.objects.filter(
-        access=Access.organization
-    ).count()
-    kwargs["total_documents_private"] = Document.objects.filter(
-        access=Access.private
-    ).count()
-    kwargs["total_documents_invisible"] = Document.objects.filter(
-        access=Access.invisible
-    ).count()
-    kwargs["total_documents_success"] = Document.objects.filter(
-        status=Status.success
-    ).count()
-    kwargs["total_documents_readable"] = Document.objects.filter(
-        status=Status.readable
-    ).count()
-    kwargs["total_documents_pending"] = Document.objects.filter(
-        status=Status.pending
-    ).count()
-    kwargs["total_documents_error"] = Document.objects.filter(
-        status=Status.error
-    ).count()
-    kwargs["total_documents_nofile"] = Document.objects.filter(
-        status=Status.nofile
-    ).count()
-    kwargs["total_documents_deleted"] = Document.objects.filter(
-        status=Status.deleted
-    ).count()
+    logger.info("[STORE STATS] Document counts")
+    document_counts = Document.objects.aggregate(
+        all=Count("pk"),
+        public=Count("pk", filter=Q(access=Access.public)),
+        private=Count("pk", filter=Q(access=Access.private)),
+        organization=Count("pk", filter=Q(access=Access.organization)),
+        invisible=Count("pk", filter=Q(access=Access.invisible)),
+        success=Count("pk", filter=Q(status=Status.success)),
+        readable=Count("pk", filter=Q(status=Status.readable)),
+        pending=Count("pk", filter=Q(status=Status.pending)),
+        error=Count("pk", filter=Q(status=Status.error)),
+        nofile=Count("pk", filter=Q(status=Status.nofile)),
+        deleted=Count("pk", filter=Q(status=Status.deleted)),
+    )
 
+    kwargs["total_documents"] = document_counts.pop("all")
+    for key, value in document_counts.items():
+        kwargs[f"total_documents_{key}"] = value
+
+    logger.info("[STORE STATS] Page counts")
     kwargs["total_pages"] = Document.objects.aggregate(
         pages=Coalesce(Sum("page_count"), 0)
     )["pages"]
@@ -77,67 +69,53 @@ def store_statistics():
         access=Access.invisible
     ).aggregate(pages=Coalesce(Sum("page_count"), 0))["pages"]
 
-    kwargs["total_notes"] = Note.objects.count()
-    kwargs["total_notes_public"] = Note.objects.filter(access=Access.public).count()
-    kwargs["total_notes_organization"] = Note.objects.filter(
-        access=Access.organization
-    ).count()
-    kwargs["total_notes_private"] = Note.objects.filter(access=Access.private).count()
-    kwargs["total_notes_invisible"] = Note.objects.filter(
-        access=Access.invisible
-    ).count()
-    kwargs["total_users_uploaded"] = (
-        Document.objects.order_by().values("user_id").distinct().count()
-    )
-    kwargs["total_users_public_uploaded"] = (
-        Document.objects.order_by()
-        .filter(access=Access.public)
-        .values("user_id")
-        .distinct()
-        .count()
-    )
-    kwargs["total_users_private_uploaded"] = (
-        Document.objects.order_by()
-        .filter(access=Access.private)
-        .values("user_id")
-        .distinct()
-        .count()
-    )
-    kwargs["total_users_organization_uploaded"] = (
-        Document.objects.order_by()
-        .filter(access=Access.organization)
-        .values("user_id")
-        .distinct()
-        .count()
-    )
-    kwargs["total_organizations_uploaded"] = (
-        Document.objects.order_by().values("organization_id").distinct().count()
-    )
-    kwargs["total_organizations_public_uploaded"] = (
-        Document.objects.order_by()
-        .filter(access=Access.public)
-        .values("organization_id")
-        .distinct()
-        .count()
-    )
-    kwargs["total_organizations_private_uploaded"] = (
-        Document.objects.order_by()
-        .filter(access=Access.private)
-        .values("organization_id")
-        .distinct()
-        .count()
-    )
-    kwargs["total_organizations_organization_uploaded"] = (
-        Document.objects.order_by()
-        .filter(access=Access.organization)
-        .values("organization_id")
-        .distinct()
-        .count()
+    logger.info("[STORE STATS] Note counts")
+    note_counts = Note.objects.aggregate(
+        all=Count("pk"),
+        public=Count("pk", filter=Q(access=Access.public)),
+        private=Count("pk", filter=Q(access=Access.private)),
+        organization=Count("pk", filter=Q(access=Access.organization)),
+        invisible=Count("pk", filter=Q(access=Access.invisible)),
     )
 
+    kwargs["total_notes"] = note_counts.pop("all")
+    for key, value in note_counts.items():
+        kwargs[f"total_notes_{key}"] = value
+
+    logger.info("User counts")
+    user_counts = Document.objects.order_by().aggregate(
+        all=Count("user_id", distinct=True),
+        public=Count("user_id", distinct=True, filter=Q(access=Access.public)),
+        private=Count("user_id", distinct=True, filter=Q(access=Access.private)),
+        organization=Count(
+            "user_id", distinct=True, filter=Q(access=Access.organization)
+        ),
+    )
+    kwargs["total_users_uploaded"] = user_counts.pop("all")
+    for key, value in user_counts.items():
+        kwargs[f"total_users_{key}_uploaded"] = value
+
+    logger.info("[STORE STATS] Organization counts")
+    org_counts = Document.objects.order_by().aggregate(
+        all=Count("organization_id", distinct=True),
+        public=Count("organization_id", distinct=True, filter=Q(access=Access.public)),
+        private=Count(
+            "organization_id", distinct=True, filter=Q(access=Access.private)
+        ),
+        organization=Count(
+            "organization_id", distinct=True, filter=Q(access=Access.organization)
+        ),
+    )
+    kwargs["total_organizations_uploaded"] = org_counts.pop("all")
+    for key, value in org_counts.items():
+        kwargs[f"total_organizations_{key}_uploaded"] = value
+
+    logger.info("[STORE STATS] Project counts")
     kwargs["total_projects"] = Project.objects.count()
 
+    logger.info("[STORE STATS] Create")
     Statistics.objects.create(**kwargs)
+    logger.info("[STORE STATS] Done")
 
 
 @periodic_task(
