7f09719c6004b78089a0c80199d97afc3a09568a
Mitchell Kotler <mitch@mitchellkotler.net>
2022-03-07 16:43:13 -0500

    more careful delete revisions

--- documentcloud/statistics/tasks.py
+++ documentcloud/statistics/tasks.py
@@ -126,7 +126,17 @@ def db_cleanup():
     logger.info("Starting DB Clean up")
     try:
         call_command("clearsessions", verbosity=2)
-        call_command("deleterevisions", days=180, verbosity=2)
+        call_command("deleterevisions", "documents", days=90, verbosity=2)
+        call_command(
+            "deleterevisions",
+            "organizations",
+            "addons",
+            "projects",
+            "statistics",
+            "users",
+            days=180,
+            verbosity=2,
+        )
     except SoftTimeLimitExceeded:
         logger.error("DB Clean up took too long")
     logger.info("Ending DB Clean up")
