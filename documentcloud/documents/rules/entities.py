8227eb3ab648816d884feeca848dd79f3d37d1fe
Jim Kang <jimkang@gmail.com>
2022-08-31 15:01:05 -0400

    Restore accidentally removed permissions to fix pre-existing tests.

--- documentcloud/documents/rules/entities.py
+++ documentcloud/documents/rules/entities.py
@@ -2,11 +2,14 @@
 
 
 # Third Party
-from rules import add_perm, always_allow, is_authenticated, predicate
+from rules import add_perm, always_allow, always_deny, is_authenticated
 
 # DocumentCloud
 from documentcloud.core.rules import skip_if_not_obj
 
-
+add_perm("documents.view_entityoccurrence", always_allow)
+add_perm("documents.add_entityoccurrence", is_authenticated)
+add_perm("documents.change_entityoccurrence", always_deny)
+add_perm("documents.delete_entityoccurrence", is_authenticated)
 add_perm("documents.view_entity", always_allow)
 add_perm("documents.add_entity", is_authenticated)
