baa02572da1b0996eb8dac55759d9fbf094f5186
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-31 17:26:47 +0000

    Adding tarfile member sanitization to extractall()

--- documentcloud/documents/processing/document_conversion/main.py
+++ documentcloud/documents/processing/document_conversion/main.py
@@ -96,7 +96,26 @@ def libre_office_convert(input_filename):
     # If not already uncompressed, uncompress
     if not os.path.exists(LIBRE_OFFICE_PATH):
         with tarfile.open(LIBRE_OFFICE_ARCHIVE, "r:gz") as tar_file:
-            tar_file.extractall(path=LIBRE_OFFICE_PATH)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_file, path=LIBRE_OFFICE_PATH)
 
     # Run LibreOffice
     # Adapted from https://github.com/vladgolubev/serverless-libreoffice/blob/master/src/libreoffice.js
