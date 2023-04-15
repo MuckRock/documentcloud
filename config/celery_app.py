d21d9dbc4df0bdaf60303b96091f738112d44790
Mitchell Kotler <mitch@mitchellkotler.net>
2023-04-15 13:54:51 -0400

    fix redis ssl options

--- config/celery_app.py
+++ config/celery_app.py
@@ -4,11 +4,19 @@ from django.conf import settings
 
 # Standard Library
 import os
+import ssl
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-app = Celery("documentcloud")
+if settings.CELERY_BROKER_URL:
+    app = Celery(
+        "documentcloud",
+        broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+        redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+    )
+else:
+    app = Celery("documentcloud")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
