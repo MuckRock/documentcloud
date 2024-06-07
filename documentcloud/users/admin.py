dcc724e8861f366b9998a50307428a2ef67e2dba
Mitchell Kotler <mitch@mitchellkotler.net>
2024-06-07 16:27:16 -0400

    user export

--- documentcloud/users/admin.py
+++ documentcloud/users/admin.py
@@ -1,5 +1,11 @@
 # Django
 from django.contrib import admin
+from django.db.models.aggregates import Count
+from django.http.response import HttpResponse
+from django.urls.conf import re_path
+
+# Standard Library
+import csv
 
 # Third Party
 from squarelet_auth.users.admin import UserAdmin as SAUserAdmin
@@ -11,3 +17,51 @@ from documentcloud.users.models import User
 @admin.register(User)
 class UserAdmin(SAUserAdmin):
     """User Admin"""
+
+    def get_urls(self):
+        """Add custom URLs here"""
+        urls = super().get_urls()
+        my_urls = [
+            re_path(
+                r"^export/$",
+                self.admin_site.admin_view(self.user_export),
+                name="users-admin-user-export",
+            ),
+        ]
+        return my_urls + urls
+
+    def user_export(self, request):
+        response = HttpResponse(
+            content_type="text/csv",
+            headers={
+                "Content-Disposition": 'attachment; filename="documentcloud_users.csv"'
+            },
+        )
+        writer = csv.writer(response)
+
+        def format_date(date):
+            if date is not None:
+                return date.strftime("%Y-%m-%d")
+            else:
+                return ""
+
+        writer.writerow(
+            ["username", "name", "email", "last_login", "date_joined", "docs"]
+        )
+        for user in (
+            User.objects.only("username", "name", "email", "last_login", "created_at")
+            .annotate(docs=Count("documents"))
+            .iterator(chunk_size=2000)
+        ):
+            writer.writerow(
+                [
+                    user.username,
+                    user.name,
+                    user.email,
+                    format_date(user.last_login),
+                    format_date(user.created_at),
+                    user.docs,
+                ]
+            )
+
+        return response
