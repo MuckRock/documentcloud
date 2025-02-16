4221670d069022e02f743bed098609c818bae39b
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-02-16 16:41:09 -0600

    Added examples for users

--- documentcloud/users/views.py
+++ documentcloud/users/views.py
@@ -10,6 +10,7 @@ import uuid
 
 # Third Party
 import django_filters
+from drf_spectacular.utils import OpenApiExample, extend_schema
 from rest_flex_fields.views import FlexFieldsMixin
 
 # DocumentCloud
@@ -32,6 +33,69 @@ class UserViewSet(
     serializer_class = UserSerializer
     queryset = User.objects.none()
     permit_list_expands = ["organization"]
+
+    @extend_schema(
+        request=None,
+        responses={200: UserSerializer(many=True)},
+        examples=[
+            OpenApiExample(
+                "List Users",
+                description="A request to retrieve a list of users.",
+                value=[
+                    {
+                        "id": 20323,
+                        "avatar_url": "",
+                        "name": "Michael Morisy",
+                        "organization": 1,
+                        "organizations": [1, 24517],
+                        "admin_organizations": [1, 24517],
+                        "username": "Granicus",
+                        "uuid": "cd6b2083-51b0-4f29-bed6-82b0bedfeb23",
+                        "verified_journalist": True,
+                    },
+                    {
+                        "id": 24282,
+                        "avatar_url": "",
+                        "name": "Michael Morisy",
+                        "organization": 3168,
+                        "organizations": [10147, 3168],
+                        "admin_organizations": [10147, 3168],
+                        "username": "MichaelMorisy_IfdAklyU",
+                        "uuid": "bed05c7e-557d-4956-a410-0218ac18d973",
+                        "verified_journalist": True,
+                    },
+                ],
+                response_only=True,
+            ),
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        request=None,
+        responses={200: UserSerializer},
+        examples=[
+            OpenApiExample(
+                "Retrieve User",
+                description="A response for a retrieve request of a specific user by ID.",  # pylint:disable=line-too-long
+                value={
+                    "id": 20323,
+                    "avatar_url": "",
+                    "name": "Michael Morisy",
+                    "organization": 1,
+                    "organizations": [1, 24517],
+                    "admin_organizations": [1, 24517],
+                    "username": "Granicus",
+                    "uuid": "cd6b2083-51b0-4f29-bed6-82b0bedfeb23",
+                    "verified_journalist": True,
+                },
+                response_only=True,
+            ),
+        ],
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
     def get_queryset(self):
         return User.objects.get_viewable(self.request.user).preload(
