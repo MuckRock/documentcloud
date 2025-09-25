f751671548f084306a720846456b36a1ee75ea10
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-09-25 08:48:40 -0500

    Try postdeploy

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
+release: ./postdeploy.sh
 web: bin/start-nginx gunicorn -c config/gunicorn.conf config.wsgi:application
 worker: REMAP_SIGTERM=SIGQUIT celery --app=config.celery_app worker --loglevel=info
 solr_worker: REMAP_SIGTERM=SIGQUIT celery --app=config.celery_app worker --loglevel=info -Q solr,celery
---
+++ postdeploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Always run migrations
+python manage.py migrate --noinput
+
+# Deploy corresponding lambda update depending on which app just deployed
+case "$HEROKU_APP_NAME" in
+  documentcloud-staging)
+    echo "Running staging Lambda update..."
+    ./config/aws/lambda/codeship_deploy_lambdas.sh staging-lambda --staging
+    ;;
+  documentcloud-prod)
+    echo "Running production Lambda update..."
+    ./config/aws/lambda/codeship_deploy_lambdas.sh prod-lambda
+    ;;
+  *)
+    echo "No matching Lambda update for app: $HEROKU_APP_NAME"
+    ;;
+esac
