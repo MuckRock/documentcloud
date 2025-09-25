cb8de5af2518c0ff3f639be6dd56dfb69e1e6a2f
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-09-25 14:29:57 -0500

    Remove lambda from postdeploy

--- postdeploy.sh
+++ postdeploy.sh
@@ -3,18 +3,3 @@ set -e
 
 # Always run migrations
 python manage.py migrate --noinput
-
-# Deploy corresponding lambda update depending on which app just deployed
-case "$HEROKU_APP_NAME" in
-  documentcloud-staging)
-    echo "Running staging Lambda update..."
-    bash "$(dirname "$0")/config/aws/lambda/codeship_deploy_lambdas.sh" staging-lambda --staging
-    ;;
-  documentcloud-prod)
-    echo "Running production Lambda update..."
-    bash "$(dirname "$0")/config/aws/lambda/codeship_deploy_lambdas.sh" prod-lambda
-    ;;
-  *)
-    echo "No matching Lambda update for app: $HEROKU_APP_NAME"
-    ;;
-esac
