097a91639fcc3ca7bf4711f9716446b30e1e4deb
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2026-09-22 14:23:28 -0500

    Add PDFSizeError message so it shows on documents/pk/errors and provide guidance

--- documentcloud/documents/processing/info_and_image/main.py
+++ documentcloud/documents/processing/info_and_image/main.py
@@ -719,7 +719,13 @@ def process_pdf(data, _context=None):
     if storage.size(doc_path) > PDF_SIZE_LIMIT:
         # If not, remove the PDF
         storage.delete(path.path(doc_id))
-        raise PdfSizeError()
+        # Display 500MB instead of 501MB so -1
+        raise PdfSizeError(
+            f"This document is larger than the "
+            f"{(PDF_SIZE_LIMIT // (1024 * 1024)) - 1}MB limit. "
+            f"Please either compress or split the PDF to conform"
+            f" to the limit."
+        )
 
     # files are always uploaded to S3 as private, set to public on S3
     # if uploaded publicly to DocumentCloud
@@ -737,7 +743,8 @@ def process_pdf(data, _context=None):
         storage.delete(path.path(doc_id))
         raise PageCountError(
             f"This document has {page_count} pages, which exceeds the "
-            f"{PAGE_COUNT_LIMIT}-page limit."
+            f"{PAGE_COUNT_LIMIT}-page limit. "
+            f"Please split the PDF before upload."
         )
 
     initialize_redis_page_data(doc_id, page_count)
